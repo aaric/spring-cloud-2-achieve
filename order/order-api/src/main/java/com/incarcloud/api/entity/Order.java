f20ed93174fe0cf0090c2ab23168d8deee64044e
qlhan <qlhan@incarcloud.com>
2020-03-31 12:26:29 +0800

    0.3.0-SNAPSHOT imports mavenBom

--- order/order-api/src/main/java/com/incarcloud/api/entity/Order.java
+++ order/order-api/src/main/java/com/incarcloud/api/entity/Order.java
@@ -1,6 +1,8 @@
 package com.incarcloud.api.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
@@ -9,9 +11,7 @@ import lombok.experimental.Accessors;
  * @author Aaric, created on 2019-12-04T11:09.
  * @version 0.0.2-SNAPSHOT
  */
-@Getter
-@Setter
-@ToString(callSuper = true)
+@Data
 @Accessors(chain = true) //支持setXxx链式编程习惯
 @NoArgsConstructor
 @AllArgsConstructor
